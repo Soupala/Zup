15d6bdc96593d475b0f088000ab27e6a9a961f25
Mica Cardillo <mica.cardillo@gmail.com>
2014-03-02 01:15:30 -0800

    Update and rename epollserver.js to app.js

---
+++ server/app.js
@@ -0,0 +1,25 @@
+Questions = new Meteor.Collection("ideas");
+
+Meteor.startup(function () {
+    // code to run on server at startup
+});
+
+Meteor.methods({
+  addQuestion : function(ideaText){
+    console.log('Adding Idea');
+    var ideaId = Idea.insert({
+          'ideaText' : ideaText,
+          'submittedOn': new Date(),
+          'submittedBy' : Meteor.userId()
+      });
+    return ideaId;
+  },
+  incrementYesVotes : function(ideaId){
+    console.log(ideaId);
+    Ideas.update(ideaId,{$inc : {'yes':1}});
+  },
+  incrementNoVotes : function(ideaId){
+    console.log(ideaId);
+    Ideas.update(ideaId,{$inc : {'no':1}});
+  }
+});
